f99d37d112877c898a8bfecd244ca46ce7cf6a12
Simon Kainz <simon@familiekainz.at>
2016-11-08 14:04:16 +0100

    adding moar files

--- proxy/index.php
+++ proxy/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$command = '/usr/bin/mosquitto_pub -h 129.27.9.27 -t "topic" -m "'.$_POST['msg'].'"';
+$command = '/usr/bin/mosquitto_pub -h 129.27.9.27 -t "topic" -m \''.$_POST['msg'].'\'';
 
 echo $command;
 echo exec($command,$o,$retval);
